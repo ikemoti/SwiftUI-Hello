6e05d466c0dfb39e044892edee3319b20d34b192
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-05-16 20:39:25 +0900

    Initial Commit

---
+++ SwiftUI Hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Hello
+//
+//  Created by Sousuke Ikemoto on 2020/05/16.
+//  Copyright © 2020 Sousuke Ikemoto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
